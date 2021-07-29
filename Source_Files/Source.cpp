aa8bb3f2b60b6418f7da33ed41bb476d1ded6c45
William <william.hayward97@yahoo.com>
2021-07-29 12:40:21 +0100

    Added calculation of angle of refraction using snells law. This involved finding the side of the prism that the ray is incident on by calculating the distance from the midpoint of all sides and taking the smallest. Then the normal vector has been predefined as we currently define our prism to be in one orientation. NOTE: the angle of refraction is not the new ray heading - this still needs to be calculated.

--- Source_Files/Source.cpp
+++ Source_Files/Source.cpp
@@ -6,13 +6,33 @@ int WINDOW_HEIGHT = 500;
 int MEDIUM_REFRACTIVE_INDEX = 1;
 float PI = 3.14159;
 
-class Prism2D
+class Medium
+{
+public:
+    int refractiveIndex;
+};
+
+float midpointBetweenCoords(float coord1, float coord2);
+
+class Prism2D : public Medium
 {
 public:
  
-    float tipXPos, tipYPos, refractiveIndex;
+    float tipXPos, tipYPos;
     float leftBaseXPos, rightBaseXPos;
     float baseYPos;
+
+    float leftSideXMidPoint;
+    float leftSideYMidPoint;
+    float leftSideNormalVectorAngleDegrees;
+    
+    float rightSideXMidPoint;
+    float rightSideYMidPoint;
+    float rightSideNormalVectorAngleDegrees;
+
+    float baseXMidPoint;
+    float baseYMidPoint;
+    float baseNormalVectorAngleDegrees;
 
     int triangeHeight;
     
@@ -26,6 +46,22 @@ public:
         leftBaseXPos = tipXPos-triangeHeight/2;
         rightBaseXPos = tipXPos+triangeHeight/2;
         baseYPos = tipYPos-triangeHeight;
+
+        // midpoint coordinates of sides will be used to find the side
+        // of the prism the ray is incident on.
+        leftSideXMidPoint = midpointBetweenCoords(leftBaseXPos, tipXPos);
+        leftSideYMidPoint = midpointBetweenCoords(baseYPos, tipYPos);
+        rightSideXMidPoint = midpointBetweenCoords(rightBaseXPos, tipXPos);
+        rightSideYMidPoint = midpointBetweenCoords(baseYPos, tipYPos);
+        baseXMidPoint = midpointBetweenCoords(leftBaseXPos, rightBaseXPos);
+        baseYMidPoint = midpointBetweenCoords(baseYPos, baseYPos);
+
+        // We know these to start with as we know the orientation we are creating
+        // the prism in our space. If we need to we can adjust these when the
+        // prism rotates.
+        leftSideNormalVectorAngleDegrees = 300.0;
+        rightSideNormalVectorAngleDegrees = 60.0;
+        baseNormalVectorAngleDegrees = 180.0;
     }
 
     // although friend function is not a member function it can
@@ -35,7 +71,7 @@ public:
     friend std::ostream& operator << (std::ostream& output, const Prism2D prism)
     {
         output << std::endl;
-        output << "********** Prism Coordinates: **********" << std::endl;
+        output << "Prism Coordinates:" << std::endl;
         output << prism.tipXPos << "," << prism.tipYPos << std::endl;
         output << prism.leftBaseXPos << "," << prism.baseYPos << std::endl;
         output << prism.rightBaseXPos << "," << prism.baseYPos << std::endl;
@@ -53,6 +89,7 @@ public:
     float rayHeadXPos, rayHeadYPos;
     int wavelenghth;
 
+    float headingDegrees;
     float headingRadians = 0.0f;
 
     // incremental change in x and y for a unit vector at
@@ -73,6 +110,7 @@ public:
         wavelenghth = lambda;
         
         // will sign of dx and dy be correct for all angles?
+        headingDegrees = thetaDegrees;
         headingRadians = (float) thetaDegrees * (PI/180);
         dx = sin(headingRadians);
         dy = cos(headingRadians);
@@ -106,17 +144,30 @@ public:
     friend std::ostream& operator << (std::ostream& output, const LightRay ray)
     {
         output << std::endl;
-        output << "********** Light Ray **********: " << std::endl;
+        output << "Light Ray: " << std::endl;
         output << "startX: " << ray.startXPos << std::endl;
         output << "startY: " << ray.startYPos << std::endl;
         output << "currentX: " << ray.rayHeadXPos << std::endl;
         output << "currentY: " << ray.rayHeadYPos << std::endl;
-        output << "Ray Heading: " << ray.headingRadians << std::endl;
+        output << "Ray Heading (Deg): " << ray.headingDegrees << std::endl;
+        output << "Ray Heading (Rad): " << ray.headingRadians << std::endl;
         output << "dx: " << ray.dx << std::endl;
         output << "dy: " << ray.dy << std::endl << std::endl;
         return output;   
     }
 };
+
+float midpointBetweenCoords(float coord1, float coord2)
+{
+    return (coord1 + coord2)/2;
+}
+
+float distanceBetweenTwoPoints(float x1, float y1, float x2, float y2)
+{
+    float dx = abs(x1-x2);
+    float dy = abs(y1-y2);
+    return pow(((dx*dx) + (dy*dy)), 0.5);
+}
 
 bool posIsInTriangle(float Ax, float Ay, float Bx, float By, float Cx, float Cy, float Px, float Py)
 {
@@ -129,12 +180,11 @@ bool posIsInTriangle(float Ax, float Ay, float Bx, float By, float Cx, float Cy,
         weights w1 and w2. P can be written in terms of A,B,C,w1,w2. This splits
         into two eqns Px and Py with two unknowns w1 and w2, which can be
         solved for.
+
+        When we take into account the fact that Ay==Cy in our case (equilateral triangle)
+        w1 and w2 expressions can be derived as:
     */
-    
-    // float w1 = ((Ax*(Cy-Ay))+((Py-Ay)*(Cx-Ax))-(Px*(Cy-Ay))) / (((By-Ay)*(Cx-Ax)) - ((Bx-Ax)*(Cy-Ay)));
-    // float w2 = (Py-Ay-(w1*(By-Ay))) / (Cy-Ay);
-    // When we take into account the fact that Ay==Cy in our case (equilateral triangle)
-    // the above w1 and w2 expressions can be derived as:
+
     double w1 = (Py-Ay)/(By-Ay);
     double w2 = ((Px-Ax)/(Cx-Ax))-(((Py-Ay)*(Bx-Ax))/((By-Ay)*(Cx-Ax)));
 
@@ -147,21 +197,112 @@ bool posIsInTriangle(float Ax, float Ay, float Bx, float By, float Cx, float Cy,
     return false;
 }
 
+float addAnglesDregreesWrapAround(float a1, float a2)
+{
+    /*
+        Simply adds two angles together, but makes sure that if it goes over
+        360deg, it wraps around to start from 0deg again.;
+    */
+
+    float addedAngle = a1 + a2;
+    if (addedAngle > 360)
+    {
+        return addedAngle - 360;
+    }
+    return addedAngle;
+    
+}
+
+float snellsLaw(LightRay ray, Prism2D prism)
+{
+    /*
+        Light travels from medium1 to medium2:
+        n1 = refractive index of medium 1
+        n2 = refractive index of medium 2
+        theta1 = angle of incident light wrt normal to boundary
+        theta2 = angle of refracted light wrt normal to boundary
+
+        Calculating the side we hit the prism on by seeing which midpoint is closest to the
+        ray and using the pre-calculated value for its normal vector angle to calculate the angle
+        of incidence. NOTE this will need to be tested close to corners of the prism.
+    */
+
+    float distances[3] = {
+        distanceBetweenTwoPoints(ray.rayHeadXPos, ray.rayHeadYPos, prism.leftSideXMidPoint, prism.leftSideYMidPoint),
+        distanceBetweenTwoPoints(ray.rayHeadXPos, ray.rayHeadYPos, prism.rightSideXMidPoint, prism.rightSideYMidPoint),
+        distanceBetweenTwoPoints(ray.rayHeadXPos, ray.rayHeadYPos, prism.baseXMidPoint, prism.baseYMidPoint)
+    };
+
+    float shortestDistance = -1.0f;
+    std::cout << "uninitialised: " << shortestDistance << std::endl;
+    for (int i = 0; i < 3; i++)
+    {
+        if (shortestDistance == -1.0)
+        {
+            shortestDistance = distances[i];
+        }
+        else if (shortestDistance)
+        {
+            if(distances[i] < shortestDistance)
+            {
+                shortestDistance = distances[i];
+            }
+        }
+
+        std::cout << "distances[i]: " << distances[i] << std::endl;
+        std::cout << "shortest: " << shortestDistance <<std::endl;
+    }
+
+    // finding the angle from vertical the normal to the surface is.
+    float normalVectorAngleDegrees;
+    if (shortestDistance == distances[0]) normalVectorAngleDegrees = prism.leftSideNormalVectorAngleDegrees;
+    if (shortestDistance == distances[1]) normalVectorAngleDegrees = prism.rightSideNormalVectorAngleDegrees;
+    if (shortestDistance == distances[2]) normalVectorAngleDegrees = prism.baseNormalVectorAngleDegrees;
+
+    // finding the angle of incidence
+    //float angleOfIncidence = std::abs(normalVectorAngleDegrees - ray.headingDegrees);
+    float angleOfIncidence = std::abs(
+        normalVectorAngleDegrees - addAnglesDregreesWrapAround(ray.headingDegrees, 180)
+    );
+
+    // putting values into rearranged snells law to give angle or refraction (wrt vertical).
+    // assuming we are going from MEDIUM to prism.
+    // Converting from degrees to randians and back on the go.
+    float angleOfRefraction = (
+        (   asin( ((float)MEDIUM_REFRACTIVE_INDEX/(float)prism.refractiveIndex) * sin(angleOfIncidence * (PI/180.0)) )   ) 
+        * (180.0/PI)
+    );
+
+    return angleOfRefraction; //angleOfRefractionDegrees;
+}
+
 int main()
 {
 
     Prism2D prism(250, 300, 50, 2);
     std::cout << prism;
+    LightRay ray(0, 275, 90, 666);
+    std::cout << ray;
     
-    LightRay ray(250, 301, 90, 666);
-    bool flag = posIsInTriangle(
-                    prism.leftBaseXPos, prism.baseYPos,
-                    prism.tipXPos, prism.tipYPos,
-                    prism.rightBaseXPos, prism.baseYPos,
-                    ray.rayHeadXPos, ray.rayHeadYPos
-    );
-    std::cout << "Pos is in Triangle: " << flag << std::endl;
+    bool rayInPrism = false;
+    while(!rayInPrism)
+    {
+        rayInPrism = posIsInTriangle(
+            prism.leftBaseXPos, prism.baseYPos,
+            prism.tipXPos, prism.tipYPos,
+            prism.rightBaseXPos, prism.baseYPos,
+            ray.rayHeadXPos, ray.rayHeadYPos
+        );
+        ray.propogate();
+    }
+    std::cout << ray;
 
+    // set refracted ray heading
+    float angleOfRefractionDegrees = snellsLaw(ray, prism);
+    ray.headingDegrees = angleOfRefractionDegrees;
+    ray.headingRadians = angleOfRefractionDegrees * (PI/180);
+
+    std::cout << ray;
 
     return 0;
 }
